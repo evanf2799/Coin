de8baf49d3c2278e19e493beeaafcc94b1f78a6c
evanf2799 <259186632+evanf2799@users.noreply.github.com>
2026-09-22 15:27:39 -0400

    work

--- Coin.java
+++ Coin.java
@@ -1,6 +1,3 @@
 public class Coin {
-    public static void main(String[] args) {
-        Coin penny = new Coin();
-        System.out.println(penny);
-    }
+
 }
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Coin penny = new Coin();
+        System.out.println(penny);
+    }
+}
