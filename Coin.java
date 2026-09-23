61a06376a2de02570137ba623361372e0c88f4ea
evanf2799 <259186632+evanf2799@users.noreply.github.com>
2026-09-23 15:19:04 -0400

    work

--- Coin.java
+++ Coin.java
@@ -1,13 +1,23 @@
 public class Coin {
     private String state;
+    private int heads;
+    private int tails;
     public String getState() {
         return state;
+    }
+    public int getHeads() {
+        return heads;
+    }
+    public int getTails() {
+        return tails;
     }
     public void flip() {
         if(Math.random() < 0.5) {
             state = "heads";
+            heads++;
         } else {
             state = "tails";
+            tails++;
         }
     }
 }
