0901b9f6a132a2af67a2cd11b71a7777bd9d6a48
evanf2799 <259186632+evanf2799@users.noreply.github.com>
2026-09-25 15:25:30 -0400

    removed most test code in Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         Coin penny = new Coin();
-        System.out.println(penny);
-        System.out.println(penny.getState());
-        penny.flip();
-        System.out.println(penny.getState());
-        System.out.println(penny.getHeads());
-        System.out.println(penny.getTails());
-        penny.flip(99);
-        System.out.println(penny.getHeads());
-        System.out.println(penny.getTails());
-        Coin nickel = new Coin(0.9);
-        nickel.flip(100);
-        System.out.println(nickel.getHeads());
-        System.out.println(nickel.getTails());
-        nickel.setPtails(0.5);
-        System.out.println(nickel.getHeads());
-        System.out.println(nickel.getTails());
     }
 }
