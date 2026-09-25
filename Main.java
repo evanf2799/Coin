115344d1bf55797209b7b4e5c7748a4a3dc9c2c3
evanf2799 <259186632+evanf2799@users.noreply.github.com>
2026-09-25 15:29:35 -0400

    work

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Coin penny = new Coin();
+        Player mrHolmer = new Player(100);
+        mrHolmer.flip(penny, "tails", 50);
+        System.out.println(mrHolmer.getBalance());
     }
 }
